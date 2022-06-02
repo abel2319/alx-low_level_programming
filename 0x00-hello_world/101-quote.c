0bfb4b664395285f5efcb91d0dc43763d31df37f
abel2319 <abelbakary@gmail.com>
2022-06-02 16:38:55 -0700

    just 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
 *  main - Entry point
 *
@@ -7,7 +7,6 @@
 */
 int main(void)
 {
-const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fwrite(msg, sizeof(msg) - 1, 1, stdout);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
