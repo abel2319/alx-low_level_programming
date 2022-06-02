cadd133e1731a9887263d929a77b94d0de8c15f1
abel2319 <abelbakary@gmail.com>
2022-06-02 13:08:09 -0700

    correction with betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,17 @@
-include<stdio.h>
- /* main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(){
-        printf("Size of a char: %ld byte(s)\nSize of an int: %ld byte(s)\nSize of a long int: %ld byte(s)\nSize of a long long int: %ld byte(s)\nSize of a float: %ld byte(s)\n", sizeof(char),sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float);
-        return 0;
+include < stdio.h >
+
+/**
+*  main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+print("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+print("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
 }
 
