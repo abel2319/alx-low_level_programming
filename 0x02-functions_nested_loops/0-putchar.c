50d8b1886494e2e7d18d236b6adbe4ff886b7309
abel2319 <abelbakary@gmail.com>
2022-06-07 14:23:46 -0700

    betty correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,8 +12,8 @@ int main(void)
 	char string[] = "_putchar";
 	size_t i = 0;
 
-	for(i = 0; i < strlen( string ); i++)
-		_putchar( string[i] );
+	for (i = 0; i < strlen(string); i++)
+		_putchar(string[i]);
 	_putchar('\n');
 	return (0);
 }
