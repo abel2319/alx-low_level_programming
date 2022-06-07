9cdc497dbaf62069f2d817c715cf1b2689f950b2
abel2319 <abelbakary@gmail.com>
2022-06-07 15:12:06 -0700

    print 10 x alphabet (2-print_alphabet_x10.c)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_alphabet - Entry to code
+*
+* Return: Always nothing (Success)
+*/
+void print_alphabet_x10(void)
+{
+	int c = 97;
+	int i = 0;
+	int j = 0;
+
+	for (j = 0; j < 10; j++)
+	{
+		i = 0;
+		c = 97;
+		while (i < 26)
+		{
+			_putchar(c + i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
