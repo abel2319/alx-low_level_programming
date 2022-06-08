4b71a4b0bf44a0ac254dd5e35e1af47cfa57e68d
abel2319 <abelbakary@gmail.com>
2022-06-07 18:49:39 -0700

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*jack_bauer - Entry to code
+*
+* Return: Always nothing (Success)
+*/
+void jack_bauer(void)
+{
+	int c = 0;
+	int j = 0;
+	int t = 0;
+	int i = 0;
+
+	for (j = 0; j < 3; j++)
+	{
+		i = 0;
+		while (i <= 9)
+		{
+			c = 0;
+			while (c <= 5)
+			{
+				t = 0;
+				while (t <= 9)
+				{
+					_putchar(j +'0');
+					_putchar(i + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(t + '0');
+					_putchar('\n');
+					t++;
+				}
+				c++;
+			}
+			i++;
+			if (j == 2 && i==4)
+				break;
+		}
+	}
+
+}
