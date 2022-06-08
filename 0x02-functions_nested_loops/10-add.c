ecdcce09483b2466b01390b121f82b26283036dc
abel2319 <abelbakary@gmail.com>
2022-06-07 20:11:58 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*add - Entry to code
+*
+* @a : first integer
+* @b : second integer
+*
+* Return: Always the sum of inputs (Success)
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
