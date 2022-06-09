25be3f05dfdc5c629597406871737ed954af0677
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-09 21:41:20 +0100

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,9 +4,9 @@
  *_isupper - Entry to code
  *@c : variable whose we verify if ti is uppercase
  *
- * Return : Always 0 or 1
+ * Return: Always 0 or 1 (Success)
  */
-int _isupper(char c)
+int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
