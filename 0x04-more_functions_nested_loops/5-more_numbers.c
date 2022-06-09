b562520c1db13260f260d4f5aca0ae821f57bd46
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-10 00:50:19 +0100

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *more_numbers - Entry to code
+ *
+ * Return: Always nothing (Success)
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 14; i++)
+	{
+		for (j = 0; j < 15; j++)
+			_putchar(i + '0');
+		_putchar('\n');
+	}
+}
