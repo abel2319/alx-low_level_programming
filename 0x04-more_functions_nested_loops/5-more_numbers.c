2f57e4f111bdfc6abda10fbdfe242fdfa825710b
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-10 00:56:59 +0100

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,15 @@ void more_numbers(void)
 	for (i = 0; i < 14; i++)
 	{
 		for (j = 0; j < 15; j++)
-			_putchar(i + '0');
+		{
+			if (j < 10)
+				_putchar( + '0');
+			else
+			{
+				_putchar('1');
+				_putchar((j % 10) + '0');
+			}
+		}
 		_putchar('\n');
 	}
 }
