ce25a08cb14c25c948e62d9e4dd3eeee71a16284
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-09 02:41:54 +0100

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci 50
+ *
+ *
+ * Return:  (Success)
+ */
+
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int f1 = 1;
+	unsigned long int f2 = 2;
+	unsigned long int next = 0;
+
+	printf("%lu", f1);
+
+	for (i = 1; i < 50; i++)
+	{
+		printf(", %lu", f2);
+		next = f1 + f2;
+        	f1 = f2;
+        	f2 = next;
+	}
+	printf("\n");
+	return (0);
+}
