18c93b83dd0ccfad5c78eb904078ad09e46d9eff
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-10 01:11:10 +0100

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_line - Entry to code
+ *@n : variable for multiplication
+ *
+ * Return: Always Nothing (Success)
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_')
+	}
+	_putchar('\n');
+}
