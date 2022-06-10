aa61e2948bc5481378f5dc22b65a732f4fd6a7da
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-10 01:20:34 +0100

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_diagonal - Entry to code
+ *@n : variable for multiplication
+ *
+ * Return: Always Nothing (Success)
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar('\');
+	}
+	_putchar('\n');
+}
