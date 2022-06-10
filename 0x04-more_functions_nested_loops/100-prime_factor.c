5d57d57f0a023f86f4fed96c5f409d3e7d31fdca
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-10 08:41:27 +0100

    100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
+/**
+ * main - Entry to code
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	long int n = 612852475143;
 	int i = 2;
+	int t = 0;
 
 	while (n != 1)
 	{
 		if (n % i == 0)
 		{
-			printf("%d ", i);
+			t = i;
 			n /= i;
 		}
 		else
 			i++;
 	}
-	printf("\n");
+	printf("%d\n", t);
 	return (0);
 }
