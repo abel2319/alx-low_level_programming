911af7e2d6b0853731537c6bf888a2f2682594d5
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-10 01:38:05 +0100

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,9 +13,11 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i < n; i++)
+		_putchar('\\');
+		_putchar('\n');
+		for (i = 1; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 1; j <= i; j++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
