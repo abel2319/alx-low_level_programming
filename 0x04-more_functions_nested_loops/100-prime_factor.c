ece74b24eb48be762b89f730633e38bfce18648a
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-10 08:38:03 +0100

    100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,20 +4,17 @@ int main(void)
 {
 	long int n = 612852475143;
 	int i = 2;
-	int t = 0;
 
 	while (n != 1)
 	{
 		if (n % i == 0)
 		{
-			t = i;
+			printf("%d ", i);
 			n /= i;
 		}
 		else
 			i++;
 	}
-
-	printf("%d\n", t);
-	
+	printf("\n");
 	return (0);
 }
