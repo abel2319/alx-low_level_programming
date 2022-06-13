8587fe3e59bfbbdb44caf8bc3529dd09a77b7612
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-13 17:03:43 +0100

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_rev - Entry code
+ *@s: pointer whose we change value
+ *
+ * Return: Always nothing (Success)
+ */
+void print_rev(char *s)
+{
+	char i = s[0];
+	int j = 0;
+
+	while (i != '\0')
+	{
+		j++;
+		i = *(s + j);
+	}
+
+	i = s[0];
+
+	while (j != 0)
+	{
+		_putchar(i);
+		j--;
+		i = *(s + j);
+	}
+	_putchar('\n');
+}
