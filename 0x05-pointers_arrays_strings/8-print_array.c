f99737753ebbf8e3c4b69ec1134ccec943cd2edf
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-13 21:04:12 +0100

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_array - Entry code
+ *@a: table to print
+ *@n: size of table
+ *
+ * Return: Always nothing (Success)
+ */
+void print_array(int *a, int n);
+{
+	int j = 1;
+
+	printf("%d", a[0]);
+	while (j < n)
+	{
+		printf(", %d", a[j]
+	}
+	printf("\n");
+}
