402882713327242f9aa80d7bcee23141bb14a507
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-13 10:32:58 +0100

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * swap_int(int *a, int *b)
+ * @a: first pointeur
+ * @b: second pointeur
+ *
+ * Return: Always nothing (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+	*a = *b;
+	*b = i;	
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0x05. C - Pointers, arrays and strings
 a function that takes a pointer to an int as parameter and updates the value it points to to 98.(0-reset_to_ç_c)
+a function that swaps the values of two integers.(1-swap.c)
