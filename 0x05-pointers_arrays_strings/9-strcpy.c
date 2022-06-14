492197e3224cea0bcc915f30508e21c049146ab6
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-14 02:02:41 +0100

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_strcpy - Entry code
+ *@dest: pointer whose we change value
+ *@src: source
+ *
+ * Return: Always char * inothing (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char i = src[0];
+	int j = 0;
+	int t = 0;
+
+	while (i != '\0')
+	{
+		j++;
+		i = *(src + j);
+	}
+	if (j != 0)
+	{
+		while (t != j)
+			dest[t] = src[t];
+			t++;
+			if (t  == j)
+			{
+				dest[t] = src[t];
+			}
+	}
+	return (dest);
+}
