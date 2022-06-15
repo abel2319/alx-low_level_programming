da36ab4faded1f5418336a67456cc4c73e454b24
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-15 13:04:33 +0100

    task100

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,3 +9,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int pw_of_ten(int n);
+int int_value(char t);
