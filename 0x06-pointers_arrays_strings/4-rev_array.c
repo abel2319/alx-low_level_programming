5ea62b0902c77c7bbe54ebcd0b4a565c873a0aa4
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-16 08:34:19 +0100

    4 review

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,18 +10,21 @@ void reverse_array(int *a, int n)
 	int i = 0;
 	int t = 0;
 	int j = n - 1;
-
-	while (t != (j / 2))
+i	
+	if (n > 0)
 	{
-		i = a[t];
-		a[t] = a[j - t];
-		a[j - t] = i;
-		t++;
-		if ((j % 2 == 1) && (t == (j / 2)))
+		while (t != (j / 2))
 		{
 			i = a[t];
 			a[t] = a[j - t];
 			a[j - t] = i;
+			t++;
+			if ((j % 2 == 1) && (t == (j / 2)))
+			{
+				i = a[t];
+				a[t] = a[j - t];
+				a[j - t] = i;
+			}
 		}
 	}
 }
