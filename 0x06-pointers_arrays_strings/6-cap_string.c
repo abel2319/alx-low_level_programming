0bd61c9158ce216387d786cd76983361aecda217
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-16 12:45:40 +0100

    6 cap review

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,28 +7,20 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+	int j = 0;
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
+	if (str[0] >= 97 && str[i] <= 122)
+	       str[0] -=32;
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ')
-			if ((str[i] >= 97) && (str[i] <= 122))
-				str[i + 1] = str[i + 1] - 32;
-
-		if (str[i] == ',' || str[i] == ';' || str[i] == '.')
-			if ((str[i] >= 97) && (str[i] <= 122))
-				str[i + 1] = str[i + 1] - 32;
-
-		else if (str[i] == '!' || str[i] == '?' || str[i] == '"')
-			if ((str[i] >= 97) && (str[i] <= 122))
-				str[i + 1] = str[i + 1] - 32;
-
-		else if (str[i] == '"' || str[i] == '(' || str[i] == ')')
-			if ((str[i] >= 97) && (str[i] <= 122))
-				str[i + 1] = str[i + 1] - 32;
-
-		else if (str[i] == '{' || str[i] == '}' || str[i] == ' ')
-			if ((str[i] >= 97) && (str[i] <= 122))
-				str[i + 1] = str[i + 1] - 32;
+		j = 0;
+		while (delimeters[j] != '\0')
+		{
+			if ((str[i] == delimeters[j]) && (str[i + 1] >= 97 && str[i + 1] <= 122))
+				str[i + 1] -= 32;
+			j++;
+		}
 		i++;
 	}
 	return (str);
