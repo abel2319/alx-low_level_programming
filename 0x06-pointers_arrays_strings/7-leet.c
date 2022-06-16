c9e33af789d22c07157f31e9913ca84dd4da62a6
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-16 14:21:22 +0100

    task7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - Entry to code
+ * @str: string whose with replace aAeEoOtTlL
+ *
+ * Return: Always nothing(Success)
+ */
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		else if (str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		else if (str[i] == 'o' || str[i] == 'O')
+			str[i] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
+		i++;
+	}
+	return (str);
+}
