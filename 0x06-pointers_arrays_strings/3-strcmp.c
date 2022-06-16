f9bd44979b5ad65c73d01dd7e8e727ab0c5c23be
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-16 09:50:22 +0100

    3 review

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,37 +11,16 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 	int j = 0;
-	int t = 0;
 
 	while (s1[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			j = (s1[i] - s2[i]);
+			break;
+		}
 		i++;
-	while (s2[t] != '\0')
-		t++;
-	if ((i < t) || (i > t))
-	{
-		while (j != i)
-		{
-			if (s1[j] > s2[2])
-				return (15);
-			if (s1[j] < s2[j])
-				return (-15);
-			j++;
-			if (j == i)
-				return (-15);
-			if (j == t)
-				return (15);
-		}
 	}
-	else
-	{
-		while (j != i + 1)
-		{
-			if (s1[j] > s2[j])
-				return (15);
-			if (s1[j] < s2[j])
-				return (-15);
-			j++;
-		}
-	}
-	return (0);
+	return (j);
 }
+
