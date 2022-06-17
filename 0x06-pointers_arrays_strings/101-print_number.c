73af40299cb3cf7d2bc5a318b85a15511ef4c848
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-17 16:22:02 +0100

    task101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+/**
+*print_number - Entry to code
+*
+* @n : integer to print
+*
+* Return: Always all number (Success)
+*/
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
+}
