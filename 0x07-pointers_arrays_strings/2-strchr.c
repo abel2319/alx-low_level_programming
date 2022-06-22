13b23776b12ce223c13ab8cac4af14b7c0f0ed1b
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-22 01:17:52 +0100

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,11 @@ char *_strchr(char *s, char c)
 	int i = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
+	{
 		if (s[i] == c)
 			return (s + i);
+		if (s[i + 1] == '\0' && s[i + 1] == c)
+			return (s + i + 1);
+	}
 	return (0);
 }
