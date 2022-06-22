c33d0a363f00a25dde1805cee08f498eb71ce3df
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-22 22:55:39 +0100

    0x08-recurcion beginning

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - print a string using only _putchar
+ *@s: string to print
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0 || *s == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
