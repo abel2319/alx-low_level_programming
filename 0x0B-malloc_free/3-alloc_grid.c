8540103e0385d85bf4e79a78ff4f26f0b9c392a1
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-27 17:23:07 +0100

    task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - build a grid of int(like a matrix)
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: Pointer to the grid [width, height]
+ * Or NULL if somethings goes wrong
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr = NULL;
+	int i = 0;
+	int j = 0;
+
+	ptr = mallac(sizeof(int *) * height);
+	if (ptr)
+	{
+		for (i = 0; i < height; i++)
+		{
+			ptr[i] = malloc(sizeof(int) * width);
+			if (!ptr[i])
+				return (NULL);
+		}
+		for (i = 0; i < height; i++)
+			for (j = 0; j < width; j++)
+				ptr[i][j] = 0;
+	}
+	return (ptr);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -2,3 +2,4 @@
 a function that creates an array of chars, and initializes it with a specific char.(0-create_array.c)
 a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.(1-strdup.c)
 a function that concatenates two strings.(2-str_concat.c)
+a function that returns a pointer to a 2 dimensional array of integers.(3-alloc_grid.c)
