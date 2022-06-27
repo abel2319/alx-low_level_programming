b6db25b229d3aa59fdbdd13fcc6ed78cc4534347
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-27 13:16:07 +0100

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,27 @@
 #include <stdlib.h>
+/**
+ * to_concat - concat
+ * @i: size of s1
+ * @j: size of s2
+ * @s1: first
+ * @s2: second
+ * @ptr: store
+ *
+ */
+void to_concat(int i, int j, char *s1, char *s2, char *ptr)
+{
+	ptr = malloc(sizeof(char) * (i + j - 1));
+	if (ptr)
+	{
+		for (t = 0; t <= (i + j - 1); t++)
+		{
+			if (t < i)
+				ptr[t] = s1[t];
+			else
+				ptr[t - i] = s2[t - i];
+		}
+	}
+}
 
 /**
  *str_concat - concatane the two strings paste as arguments
@@ -14,6 +37,7 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 	int t = 0;
+	char *rtn = '\0';
 
 	if (s1 && s2)
 	{
@@ -21,17 +45,21 @@ char *str_concat(char *s1, char *s2)
 			;
 		for (j = 0; s2[j] != '\0'; j++)
 			;
-		ptr = malloc(sizeof(char) * (i + j - 1));
-		if (ptr)
-		{
-			for (t = 0; t <= (i + j - 1); t++)
-			{
-				if (t < i)
-					ptr[t] = s1[t];
-				else
-					ptr[t - i] = s2[t - i];
-			}
-		}
+		to_concat(i, j, s1, s2, ptr);
 	}
+	else if (s1 && !s2)
+	{
+		for (i = 0; s1[1] != '\0'; i++)
+			;
+		to_concat(i, 0, s1, s2, ptr);
+	}
+	else if (!s1 && s2)
+	{
+		for (j = 0; s2[1] != '\0'; j++)
+			;
+		to_concat(j, 0, s2, s1, ptr);
+	}
+	else
+		ptr = rtn;
 	return (ptr);
 }
