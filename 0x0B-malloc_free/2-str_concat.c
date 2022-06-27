1f5d8441215a3c4883186b2506a0ca5d64bbbc34
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-27 16:44:59 +0100

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,19 +8,24 @@
  * @ptr: store
  *
  */
-void to_concat(int i, int j, char *s1, char *s2, char *ptr)
+void to_concat(int i, int j, char *s1, char *s2, char **ptr)
 {
 	int t = 0;
 
-	ptr = malloc(sizeof(char) * (i + j - 1));
+	(*ptr) = malloc(sizeof(char) * (i + j));
 	if (ptr)
 	{
 		for (t = 0; t <= (i + j - 1); t++)
 		{
-			if (t < i)
-				ptr[t] = s1[t];
+			if (t <= i)
+			{
+				if (t == i)
+					(*ptr)[t] = s2[0];
+				else
+					(*ptr)[t] = s1[t];
+			}
 			else
-				ptr[t - i] = s2[t - i];
+				(*ptr)[t] = s2[t - i];
 		}
 	}
 }
@@ -40,25 +45,25 @@ char *str_concat(char *s1, char *s2)
 	int j = 0;
 	char *rtn = '\0';
 
-	if (s1 && s2)
+	if (s1 != NULL && s2 != NULL)
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 			;
 		for (j = 0; s2[j] != '\0'; j++)
 			;
-		to_concat(i, j, s1, s2, ptr);
+		to_concat(i, j, s1, s2, &ptr);
 	}
-	else if (s1 && !s2)
+	else if (s1 != NULL && s2 == NULL)
 	{
-		for (i = 0; s1[1] != '\0'; i++)
+		for (i = 0; s1[i] != '\0'; i++)
 			;
-		to_concat(i, 0, s1, s2, ptr);
+		to_concat(i, 0, s1, '\0', &ptr);
 	}
-	else if (!s1 && s2)
+	else if (s1 == NULL && s2 != NULL)
 	{
-		for (j = 0; s2[1] != '\0'; j++)
+		for (j = 0; s2[j] != '\0'; j++)
 			;
-		to_concat(j, 0, s2, s1, ptr);
+		to_concat(j, 0, s2, '\0', &ptr);
 	}
 	else
 		ptr = rtn;
