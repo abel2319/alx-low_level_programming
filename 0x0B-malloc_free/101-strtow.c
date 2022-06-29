e70b1af21bfe157e6599daf77190635c3f214707
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-29 09:05:41 +0100

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -25,7 +25,7 @@ char **allocate(char *str)
 	}
 	if (nbr_words == 0)
 		return (NULL);
-	ptr = malloc(sizeof(char *) * (nbr_words + 2));
+	ptr = malloc(sizeof(char *) * nbr_words);
 	return (ptr);
 }
 
@@ -98,7 +98,7 @@ char **strtow(char *str)
 					}
 					if (j != 0)
 					{
-					ptr[count] = malloc(sizeof(char) * (j + 1));
+					ptr[count] = malloc(sizeof(char) * (j + 2));
 					if (!ptr[count])
 					{
 						failed(count, ptr);
