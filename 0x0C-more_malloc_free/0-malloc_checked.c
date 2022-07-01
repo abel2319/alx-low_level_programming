ecd868e4cf30e9c6b61189ab95dda150dc357787
abel2319 <abel.kpohinto@imsp-uac.org>
2022-07-01 01:51:28 +0100

    more malloc beginning

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include < stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using a size past as argument
+ * @b: size of memory
+ *
+ * Return: the pointer to the new space of memory allocated
+ * Or 98 if allocation failed
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = 98;
+
+	if (b != 0)
+	{
+		ptr = malloc(b);
+		if(!ptr)
+			return (98);
+	}
+	return (ptr);
+}
