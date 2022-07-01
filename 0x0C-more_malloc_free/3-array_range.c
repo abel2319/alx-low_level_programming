11c45095155d416d4d59f5d8b64c7788ff2ee903
abel2319 <abel.kpohinto@imsp-uac.org>
2022-07-01 03:23:29 +0100

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,15 +11,17 @@
 int *array_range(int min, int max)
 {
 	int i = 0;
-
 	int *ptr = NULL;
 
 	if (min > max)
 		return (NULL);
 
 	ptr = malloc(sizeof(int) * (max - min + 1));
+
 	if (ptr)
-		for (i = min; i <= max; i++)
-			*(ptr + i) = i;
+		return (NULL);
+
+	for (i = min; i <= max; i++)
+		*(ptr + i) = i;
 	return (ptr);
 }
