55f126cd684d97e9a0b06116a01dd92f3405ace9
abel2319 <abel.kpohinto@imsp-uac.org>
2022-07-01 03:55:09 +0100

    task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr1);
 	}
 
-	ptr1 = mallloc(new_size);
+	ptr1 = malloc(new_size);
 	if (ptr1)
 	{
 		for (i = 0; i < (int)old_size; i++)
