00390347eda9cfc4241bee848c870e91c0e1c79d
abel2319 <abel.kpohinto@imsp-uac.org>
2022-07-04 20:09:24 +0100

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,14 +11,14 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (name)
-		pritnf("%s\n", (*d).name);
+			pritnf("%s\n", d.name);
 		else
 			printf("nil\n");
 
 		printf("%lf\n", (*d).age);
 
 		if (owner)
-			printf("%s\n", (*d).owner);
+			printf("%s\n", d.owner);
 		else
 			printf("nil\n");
 	}
