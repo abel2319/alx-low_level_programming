528b734c67fd807927224bc2189fb1310d5d7a73
abel2319 <abel.kpohinto@imsp-uac.org>
2022-07-04 22:01:21 +0100

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
 /**
  * new_dog - create a new dog
  * @name: name of the new dog
@@ -16,7 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		ptr_dog->name = name;
 		ptr_dog->owner = owner;
-		(*ptr).age = age;
+		(*ptr_dog).age = age;
 		return (ptr_dog);
 	}
 	return (ptr_dog);
