bc650842246ba6fa33e0a20cc1c759bc66f591c9
abel2319 <abel.kpohinto@imsp-uac.org>
2022-07-04 20:07:54 +0100

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print informatrions about a dog d
+ * @d : whose we print informations
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (name)
+		pritnf("%s\n", (*d).name);
+		else
+			printf("nil\n");
+
+		printf("%lf\n", (*d).age);
+
+		if (owner)
+			printf("%s\n", (*d).owner);
+		else
+			printf("nil\n");
+	}
+}
