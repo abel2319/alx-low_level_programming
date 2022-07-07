f47ef2119095d570c3473ae9efe7a7c424d1a3cd
abel2319 <abel.kpohinto@imsp-uac.org>
2022-07-07 06:32:32 +0100

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * array_iterator - executes an action to the elements of an array
  * @array: array of elements
@@ -6,7 +7,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i = 0;
+	size_t i = 0;
 
 	for (i = 0; i < size; i++)
 	{
