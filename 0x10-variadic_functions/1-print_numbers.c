df748cf4c116a743f0991474f4701675edd2393c
abel2319 <abelbakary@gmail.c>
2022-07-10 18:58:49 +0100

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(li, n);
 	if (n != 0)
 	{
-		if (separato != NULL)
+		if (separator != NULL)
 		{
 			for (i = 0; i < (n -1); i++)
 				printf("%d%s", va_arg(li, int), separator);
