641036478507a68daf33654af9b08f511a31260d
merino <merino@merino.imsp.org>
2022-07-15 00:45:41 +0100

    beginning with task0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - print elts in a singly linked list
+ * @n: first elt of the list
+ *
+ * Return: the number of elts in the list
+ */
+size_t print_list(const list_t *n)
+{
+	size_t nbr = 0;
+
+	if (n == NULL)
+		return (0);
+	while (n)
+	{
+		if (n->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%u] %s\n", n->len, n->str);
+		}
+		n = n->next;
+		++nbr
+	}
+	return (nbr);
+}
