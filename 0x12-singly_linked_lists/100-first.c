16256d79f5b73f68f14973169a64fcb6cacc24b2
abel2319 <abelbakary@gmail.com>
2022-07-15 07:36:57 +0100

    advanced tasks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * print_before_main - this function execute itself before the mqin function
- * and without have been called
+ * print_before_main - this function executes itsef
+ * before the main function and without have been called
  *
  */
-void print_before_main (void) __attribute__ ((constructor));
+void print_before_main(void) __attribute__((constructor));
 void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
