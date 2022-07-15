02aae89137b4cc78cca5eadeb3f457f929413797
abel2319 <abelbakary@gmail.com>
2022-07-15 07:40:58 +0100

    advanced tasks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,9 @@
  * before the main function and without have been called
  *
  */
-void print_before_main(void) __attribute__((constructor));
+void print_before_main(void) __attribute__ ((constructor));
 void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
