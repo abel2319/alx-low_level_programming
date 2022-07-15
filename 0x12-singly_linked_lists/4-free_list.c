af175454b253326d5923eceeee8826d61fd9fca7
abel2319 <abelbakary@gmail.com>
2022-07-15 02:13:37 +0100

    task4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,37 @@
 #include "lists.h"
 
-
+/**
+ * free_list - free list in head
+ * @head: address of header of list
+ *
+ */
 void free_list(list_t *head)
 {
 	list_t *tmp1, *tmp2;
 
-	if (*head)
+	if (head)
 	{
-		tmp1 = *head;
-		tmp2 = tmp1->next;
-		while (tmp2)
+		if (head->next == NULL)
 		{
-			free(tmp1);
-			tmp1 = tmp2;
-			tmp2 = tmp1->next;
+			free(head->str);
+			free(head);
 		}
-		free(tmp1);
-		free(head);
+		else
+		{
+			tmp1 = head;
+			tmp2 = tmp1->next;
+			while (tmp2)
+			{
+				free(tmp1->str);
+				free(tmp1);
+				tmp1 = tmp2;
+				tmp2 = tmp1->next;
+				if (tmp2 == NULL)
+				{
+					free(tmp1->str);
+					free(tmp1);
+				}
+			}
+		}
 	}
 }
