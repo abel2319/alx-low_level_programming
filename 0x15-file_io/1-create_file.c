42edcfb8a3454a2f28d6ffe836a90fc9e7a0e311
abel2319 <abelbakary@gmail.com>
2022-07-27 04:11:38 +0100

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 		for (i = 0; text_content[i]; i++)
 			;
-	op = open(filename, O_RONLY | O_RDWR | O_TRUNC, 0600);
+	op = open(filename, O_RDONLY | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, i);
 	if (op == -1 || wr == -1)
 		return (-1);
