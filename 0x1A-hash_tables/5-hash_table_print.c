91e60b1e355fe91485c4fe3f0827faf7f52d45b3
abel2319 <abelbakary@gmail.com>
2022-09-24 14:54:47 +0100

    task4 correction

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 				while (node != NULL)
 				{
 				coma = 1;
-				printf("'%s': '%s'",node->key, node->value);
+				printf("'%s': '%s'", node->key, node->value);
 				if (node->next != NULL)
 					printf(", ");
 				node = node->next;
--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -12,30 +12,26 @@ int main(void)
 {
     
     hash_table_t *ht;
-    char *key;
     char *value;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "c", "fun");
-    hash_table_set(ht, "python", "awesome");
-    hash_table_set(ht, "Bob", "and Kris love asm");
-    hash_table_set(ht, "N", "queens");
-    hash_table_set(ht, "Asterix", "Obelix");
-    hash_table_set(ht, "Betty", "Cool");
-    hash_table_set(ht, "98", "Battery Streetz");
-    key = strdup("Tim");
-    value = strdup("Britton");
-    hash_table_set(ht, key, value);
-    key[0] = '\0';
-    value[0] = '\0';
-    free(key);
-    free(value);
-    hash_table_set(ht, "98", "Battery Street"); 
-    hash_table_set(ht, "hetairas", "Bob");
-    hash_table_set(ht, "hetairas", "Bob Z");
-    hash_table_set(ht, "mentioner", "Bob");
-    hash_table_set(ht, "hetairas", "Bob Z Chu");
-    hash_table_print(ht);
-    hash_table_delete(ht);
+
+	ht = hash_table_create(1024);
+	hash_table_set(ht, "USA", "Washington");
+	hash_table_set(ht, "France", "Paris");
+	hash_table_set(ht, "UK", "London");
+	hash_table_set(ht, "Spain", "Madrid");
+	hash_table_set(ht, "Germany", "Berlin");
+	hash_table_set(ht, "China", "Beijing");
+	hash_table_set(ht, "Russia", "Moscow");
+	hash_table_set(ht, "Bahrain", "Manama");
+	hash_table_set(ht, "Canada", "Ottawa");
+	hash_table_set(ht, "Brazil", "Brasilia");
+	hash_table_set(ht, "Sweden", "Stockholm");
+	hash_table_set(ht, "Iceland", "Reykjavik");
+	hash_table_set(ht, "Japan", "Tokyo");
+	hash_table_set(ht, "Australia", "Canberra");
+
+	value = hash_table_get(ht, "USA");
+	printf("%s:%s\n", "USA", value);
     return (EXIT_SUCCESS);
 }
