47e20dd2c690d50753b88f912cac535d54f5748f
abel2319 <abelbakary@gmail.com>
2022-09-24 03:54:36 +0100

    task4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get -  retrieves a value associated with a key.
+ * @ht: hash table
+ * @key: the key
+ *
+ * Return: the value, NULL otherwise
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+
+	if (ht != NULL && ht->array != NULL && key != NULL)
+	{
+		index = hash_djb((unsigned char *)key, ht->size);
+		if (index < ht->size)
+			return (ht->array[index]->value);
+	}
+	return (NULL);
+}
