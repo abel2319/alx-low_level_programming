8896aecfd6b6890f83ab3206bdf160cac19aba81
abel2319 <abelbakary@gmail.com>
2022-11-11 10:51:17 +0100

    task16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - fibonacci 50
+ * main - fibonacci 98
  *
  *
  * Return:  (Success)
@@ -13,15 +13,39 @@ int main(void)
 	unsigned long int f1 = 1;
 	unsigned long int f2 = 2;
 	unsigned long int next = 0;
+	unsigned long int f_half1, f_half2, l_half1, l_half2, part1, part2;
 
 	printf("%lu", f1);
 
-	for (i = 1; i <= 98; i++)
+	for (i = 1; i < 93; i++)
 	{
 		printf(", %lu", f2);
 		next = f1 + f2;
 		f1 = f2;
 		f2 = next;
+	}
+	
+	f_half1 = f1 / 10000000000;
+	f_half2 = f2 / 10000000000;
+	l_half1 = f1 % 10000000000;
+	l_half2 = f2 % 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		part1 = f_half1 + f_half2;
+		part2 = l_half1 + l_half2;
+		if (l_half2 + l_half2 > 9999999999)
+		{
+			part1 += 1;
+			part2 %= 10000000000;
+		}
+
+		printf(", %lu%lu", part1, part2);
+
+		f_half1 = l_half1;
+		f_half2 = l_half2;
+		l_half1 = part1;
+		l_half2 = part2;
 	}
 	printf("\n");
 	return (0);
