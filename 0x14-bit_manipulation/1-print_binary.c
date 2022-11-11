730c4a985f28705f3def665bc1c423c95b92ff21
abel2319 <abelbakary@gmail.com>
2022-11-11 16:21:28 +0100

    task2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,14 +8,14 @@ void print_binary(unsigned long int n)
 	unsigned int i = 0;
 
 	if (n == 1 || n == 0)
+	{
 		_putchar('0' + n);
+		return
+	}
 	else
 	{
-		i = 2;
-		while ((i * 2) < n)
-			i++;
-		_putchar('0' + (i - n));
-		n = n - i;
+		_putchar('0' + (n % 2));
+		n = (n - (n % 2)) / 2;
 		print_binary(n);
 	}
 }
